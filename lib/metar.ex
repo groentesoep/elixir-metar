0777b15a96154a2997703a6e9191927ed9d9dbd2
Bastiaan <lbveldjesgraaf@gmail.com>
2017-10-08 11:14:29 +0200

    Inital commit.

---
+++ lib/metar.ex
@@ -0,0 +1,18 @@
+defmodule Metar do
+  @moduledoc """
+  Documentation for Metar.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Metar.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/metar_test.exs
@@ -0,0 +1,8 @@
+defmodule MetarTest do
+  use ExUnit.Case
+  doctest Metar
+
+  test "greets the world" do
+    assert Metar.hello() == :world
+  end
+end
